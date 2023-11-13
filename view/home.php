8315293d1651338046e052943a95577470c9c5c5
Nguyễn Văn Dương <nguyenduong0782004@gmail.com>
2023-11-13 23:10:10 +0700

    main

--- view/home.php
+++ view/home.php
@@ -16,7 +16,30 @@
                     <i class="fa-solid fa-location-crosshairs"></i> Tra cứu chuyến bay
                 </span>
             </div>
-            <input type="radio"> Khứ hồi
+            <div class="option">
+                <label class="radio">
+                    <span>
+                        <input name="gander" type="radio"> Khứ Hồi
+                    </span>
+
+                    <span>
+                        <input name="gander" type="radio"> Một chiều
+                    </span>
+                </label>
+            </div>
+            <div class="Airlines">
+                <div class="start">
+                    <i class="fa-solid fa-jet-fighter"></i>
+                    <input type="text" placeholder=" Điểm khởi hành"> <span>Ngày đi</span>
+                    <input type="date">
+                </div>
+                <div class="Arrive">
+                    <i class="fa-solid fa-plane-circle-check"></i>
+                    <input type="text" placeholder=" Điểm đến"> <span>Ngày về</span>
+                    <input type="date">
+                </div>
+                <input type="submit" value="Tìm chuyến bay">
+            </div>
         </div>
     </form>
 </div>

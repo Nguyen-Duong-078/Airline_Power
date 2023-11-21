dc546f940008f65e3008dfa873a6c027efe42754
Nguyễn Văn Dương <nguyenduong0782004@gmail.com>
2023-11-22 00:03:12 +0700

    dương

--- view/Client/login.php
+++ view/Client/login.php
@@ -1,95 +1,63 @@
-        <!--~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-        Start Banner
-    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~-->
-        <section class="banner-section inner-banner-section bg-overlay-black bg_img" data-background="assets/images/bg/inner-bg.png">
-            <div class="container-fluid">
-                <div class="row justify-content-center align-items-center">
-                    <div class="col-xl-12 text-center">
-                        <div class="banner-content">
-                            <h1 class="title">Sign-In</h1>
-                            <div class="breadcrumb-area">
-                                <nav aria-label="breadcrumb">
-                                    <ol class="breadcrumb">
-                                        <li class="breadcrumb-item"><a href="index.php">Trang Chủ</a></li>
-                                        <li class="breadcrumb-item active" aria-current="page">Sign-In</li>
-                                    </ol>
-                                </nav>
+<!-- Start Account-->
+
+<section class="account-section bg--gray ptb-120">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-xl-12 text-center">
+                <div class="section-header">
+                    <h2 class="section-title">Đăng nhập Tài Khoản</h2>
+                </div>
+            </div>
+        </div>
+        <div class="row justify-content-center">
+            <div class="col-xl-5 col-lg-6">
+                <div class="account-form-area">
+                    <form class="account-form" action="index.php?action=login" method="post">
+                        <div class="row mb-10-none">
+                            <div class="col-xl-12 form-group">
+                                <label class="icon"><i class="icon-name_icone"></i></label>
+                                <input type="text" class="form--control" name="username" placeholder="Tài khoản" required>
+                            </div>
+                            <div class="col-xl-12 form-group">
+                                <label class="icon"><i class="icon-Create_Password_icone"></i></label>
+                                <input type="password" class="form--control" name="password" placeholder="password" required>
+                            </div>
+                            <div class="col-lg-12 form-group text-center">
+                                <input type="submit" class="btn--base w-100" name="login" value="     Đăng Nhâp" style="color:black">
+                            </div>
+                            <div class="col-lg-12 text-center">
+                                <div class="account-item mt-10">
+                                    <label><a href="index.php?action=forgot" class="text--base"> Quên Mật Khẩu</a></label>
+                                </div>
+                            </div>
+                            <div class="col-lg-12 text-center">
+                                <div class="or mt-10">
+                                    <label>Hoặc</label>
+                                </div>
+                            </div>
+                            <div class="col-lg-12 form-group google-submit text-center">
+                                <button type="submit" class="btn--base w-100"><span class="mr-2"><img src="assets/images/google.png" alt="google"></span> Tiếp Tục Với
+                                    Google</button>
+                            </div>
+                            <div class="col-lg-12 text-center">
+                                <div class="account-item mt-10">
+                                    <label>Chưa có tài khoản <a href="index.php?action=register" class="text--base"> Tạo một tài khoản</a></label>
+                                </div>
+                                <div class="thongbao">
+                                    <h2>
+                                        <?php
+                                        if (isset($Thongbao) && ($Thongbao != ""))
+                                            echo $Thongbao;
+                                        ?>
+                                    </h2>
+                                </div>
                             </div>
                         </div>
-                    </div>
+                    </form>
                 </div>
             </div>
-        </section>
-        <!--~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-        End Banner
-    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~-->
+        </div>
+    </div>
+</section>
 
-        <!--~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-        Start Scroll-To-Top
-    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~-->
-        <a href="#" class="scrollToTop">
-            <i class="las la-dot-circle"></i>
-            <span>Top</span>
-        </a>
-        <!--~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-        End Scroll-To-Top
-    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~-->
-
-        <!--~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-        Start Account
-    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~-->
-        <section class="account-section bg--gray ptb-120">
-            <div class="container">
-                <div class="row justify-content-center">
-                    <div class="col-xl-12 text-center">
-                        <div class="section-header">
-                            <!-- <span class="sub-title"><span>Sign In</span></span> -->
-                            <h2 class="section-title">Đăng nhập Tài Khoản</h2>
-                        </div>
-                    </div>
-                </div>
-                <div class="row justify-content-center">
-                    <div class="col-xl-5 col-lg-6">
-                        <div class="account-form-area">
-                            <form class="account-form" action="login.php" method="post">
-                                <div class="row mb-10-none">
-                                    <div class="col-xl-12 form-group">
-                                        <label class="icon"><i class="las la-envelope"></i></label>
-                                        <input type="email" class="form--control" name="email" placeholder="Email" required>
-                                    </div>
-                                    <div class="col-xl-12 form-group">
-                                        <label class="icon"><i class="icon-Create_Password_icone"></i></label>
-                                        <input type="password" class="form--control" name="password" placeholder="Create Password" required>
-                                    </div>
-                                    <div class="col-lg-12 form-group text-center">
-                                        <button type="submit" class="btn--base w-100">Đăng Nhập</button>
-                                    </div>
-                                    <div class="col-lg-12 text-center">
-                                        <div class="account-item mt-10">
-                                            <label><a href="index.php?action=forgot" class="text--base"> Quên Mật Khẩu</a></label>
-                                        </div>
-                                    </div>
-                                    <div class="col-lg-12 text-center">
-                                        <div class="or mt-10">
-                                            <label>Hoặc</label>
-                                        </div>
-                                    </div>
-                                    <div class="col-lg-12 form-group google-submit text-center">
-                                        <button type="submit" class="btn--base w-100"><span class="mr-2"><img src="assets/images/google.png" alt="google"></span> Tiếp Tục Với
-                                            Google</button>
-                                    </div>
-                                    <div class="col-lg-12 text-center">
-                                        <div class="account-item mt-10">
-                                            <label>Chưa có tài khoản <a href="index.php?action=register" class="text--base"> Tạo một tài khoản</a></label>
-                                        </div>
-                                    </div>
-                                </div>
-                            </form>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section>
-        <!--~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-        End Account
-    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~-->
+<!--End Account-->
